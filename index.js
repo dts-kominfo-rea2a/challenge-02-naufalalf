7ef26e9e2246abcb9de91331386f2d468218359b
naufalalfikri <naufalalfikri@gmail.com>
2022-05-29 19:32:54 +0700

    Sudah dikerjakan untuk tugas 06

--- index.js
+++ index.js
@@ -23,20 +23,26 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
-
+  let hasilLooping = [];
+  for( let pegawai of arrPegawai ){
+    hasilLooping.push(`${pegawai.namaDepan} ${pegawai.namaBelakang}`);
+  }
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
-
+  let jumlahPria = 0;
+  for( let pegawai of arrPegawai ){
+    pegawai.jenisKelamin === 'M' && jumlahPria++;
+  }
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
-
+  let jumlahWanita = 0;
+  for( let pegawai of arrPegawai ){
+    pegawai.jenisKelamin === 'F' && jumlahWanita++;
+  }
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -49,7 +55,14 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
-
+  if(jumlahPria == jumlahWanita){
+    komentar = "Jumlah Pria dan Wanita sama";
+  }else if(jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  }else if(jumlahPria > jumlahWanita){
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  }
+  
   // ! JANGAN DIMODIFIKASI
   return {
     hasilLooping,
